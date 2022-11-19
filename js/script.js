457522677a60da10c69615671a338dd15baeab00
Anand <hyeiamaka06@gmail.com>
2022-11-20 04:14:11 +0530

    Mobile tab Navigation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const content = document.querySelectorAll('.image');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((item,idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents();
+        hideAllItems();
+
+        item.classList.add('active');
+        content[idx].classList.add('opened');
+    })
+})
+
+function hideAllItems() {
+    buttons.forEach(item => item.classList.remove('active'));
+}
+function hideAllContents() {
+    content.forEach(content => content.classList.remove('opened'));
+}
